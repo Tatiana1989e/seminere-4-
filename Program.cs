135859362480a1e2378bc8fb13065280187e3cf7
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-17 19:09:30 +0300

    seminars4homework task27

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿/*
+// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 int Exponentiation(int numberA, int numberB)
 {
@@ -18,3 +19,19 @@ int Exponentiation(int numberA, int numberB)
 
   int degree = Exponentiation(numberA, numberB);
   Console.WriteLine("Number A in a power B: " + degree);
+*/
+/*
+ // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Input number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("sum of all the digits in the number: " + sum);
+*/
